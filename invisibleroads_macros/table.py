2a07e90de2dc6e91d7a36907f2cbf6d6fa3bffdc
Roy Hyunjin Han <rhh2109@columbia.edu>
2016-09-29 17:18:07 -0400

    Update normalize_column_name

--- invisibleroads_macros/table.py
+++ invisibleroads_macros/table.py
@@ -5,8 +5,8 @@ from .text import compact_whitespace
 
 UPPER_LOWER_PATTERN = re.compile(r'(.)([A-Z][a-z]+)')
 LOWER_UPPER_PATTERN = re.compile(r'([a-z0-9])([A-Z])')
-DIGIT_LETTER_PATTERN = re.compile(r'([0-9])([a-z])')
 LETTER_DIGIT_PATTERN = re.compile(r'([a-z])([0-9])')
+DIGIT_LETTER_PATTERN = re.compile(r'([0-9])([a-z])')
 
 
 def duplicate_selected_column_names(selected_column_names, column_names):
@@ -17,7 +17,11 @@ def duplicate_selected_column_names(selected_column_names, column_names):
     return list(column_names) + [x + suffix for x in selected_column_names]
 
 
-def normalize_column_name(column_name, word_separator=' '):
+def normalize_column_name(
+        column_name,
+        word_separator=' ',
+        separate_camel_case=False,
+        separate_letter_digit=False):
     """
     Normalize name variations, using a variation of the method described in
     http://stackoverflow.com/a/1176023/192092
@@ -26,13 +30,17 @@ def normalize_column_name(column_name, word_separator=' '):
     OneTwo   one two
     one-two  one two
     one_two  one two
-    1two     1 two
     one2     one 2
+    1two     1 two
     """
-    s = UPPER_LOWER_PATTERN.sub(r'\1 \2', column_name)
-    s = LOWER_UPPER_PATTERN.sub(r'\1 \2', s).lower()
-    s = DIGIT_LETTER_PATTERN.sub(r'\1 \2', s)
-    s = LETTER_DIGIT_PATTERN.sub(r'\1 \2', s)
+    s = column_name
+    if separate_camel_case:
+        s = UPPER_LOWER_PATTERN.sub(r'\1 \2', s)
+        s = LOWER_UPPER_PATTERN.sub(r'\1 \2', s)
+    s = s.lower()
+    if separate_letter_digit:
+        s = LETTER_DIGIT_PATTERN.sub(r'\1 \2', s)
+        s = DIGIT_LETTER_PATTERN.sub(r'\1 \2', s)
     word_separators = ['-', '_', ' ']
     if word_separator not in word_separators:
         word_separators.append(word_separator)
