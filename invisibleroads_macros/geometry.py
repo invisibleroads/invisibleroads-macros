cc7fedd42b63c3f99823aff7e4fa3180545e93a5
Roy Hyunjin Han <rhh@crosscompute.com>
2016-10-03 12:36:48 -0400

    Add flip_geometry_coordinates

---
+++ invisibleroads_macros/geometry.py
@@ -0,0 +1,20 @@
+from copy import copy
+
+
+def flip_geometry_coordinates(geometries):
+    flipped_geometries = []
+    for geometry in geometries:
+	flipped_geometry = copy(geometry)
+        if hasattr(geometry, 'geoms'):
+            flipped_geometry.geoms = flip_geometry_coordinates(geometry.geoms)
+        else:
+            flipped_geometry.coords = [flip_xy(xyz) for xyz in geometry.coords]
+    flipped_geometries.append(flipped_geometry)
+    return flipped_geometries
+
+
+def flip_xy(xyz):
+    'Flip x and y coordinates whether or not there is a z-coordinate'
+    xyz = list(xyz)  # Preserve original
+    xyz[0], xyz[1] = xyz[1], xyz[0]
+    return xyz
