33ea0a157c965626e1b504c344a151df07f52795
Roy Hyunjin Han <rhh@crosscompute.com>
2017-02-14 18:37:21 -0500

    Define has_whitespace

--- invisibleroads_macros/text.py
+++ invisibleroads_macros/text.py
@@ -5,17 +5,21 @@ WHITESPACE_PATTERN = re.compile(r'\s+', re.MULTILINE)
 PUNCTUATION_PATTERN = re.compile(r'[^a-zA-Z\s]+')
 
 
-def parse_words(x):
-    return x.replace(',', ' ').split()
-
-
-def strip_whitespace(string):
-    return WHITESPACE_PATTERN.sub('', string)
+def has_whitespace(string):
+    return WHITESPACE_PATTERN.search(string) is not None
 
 
 def compact_whitespace(string):
     return WHITESPACE_PATTERN.sub(' ', string).strip()
 
 
+def strip_whitespace(string):
+    return WHITESPACE_PATTERN.sub('', string)
+
+
 def remove_punctuation(string):
     return compact_whitespace(PUNCTUATION_PATTERN.sub(' ', string))
+
+
+def parse_words(x):
+    return x.replace(',', ' ').split()
