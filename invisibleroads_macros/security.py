9394dc75aa390ba66ca2682d21db8dd5c6d7772b
Roy <rhh@crosscompute.com>
2017-06-20 23:42:21 -0400

    Use random.SystemRandom because it is more random

--- invisibleroads_macros/security.py
+++ invisibleroads_macros/security.py
@@ -6,6 +6,7 @@ try:
     ALPHABET = string.digits + string.letters
 except AttributeError:
     ALPHABET = string.digits + string.ascii_letters
+RANDOM = random.SystemRandom()
 
 
 def make_random_string(
@@ -14,4 +15,4 @@ def make_random_string(
         alphabet += string.punctuation
     if with_spaces:
         alphabet += ' '
-    return ''.join(random.choice(alphabet) for x in range(length))
+    return ''.join(RANDOM.choice(alphabet) for x in range(length))
