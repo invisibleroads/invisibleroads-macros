36793b6d59fa21987b178de16fc2ce1f66e733b2
Roy <rhh@crosscompute.com>
2017-08-02 09:50:10 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='invisibleroads-macros',
-    version='0.8.4',
+    version='0.8.4.1',
     description='Shortcut functions',
     long_description=DESCRIPTION,
     classifiers=[
