566007bc4b540236774a460f99f35e9290de4182
Roy Hyunjin Han <rhh@crosscompute.com>
2018-02-15 11:29:20 -0500

    Define load_csv_safely

--- invisibleroads_macros/table.py
+++ invisibleroads_macros/table.py
@@ -1,4 +1,5 @@
 import re
+from functools import partial
 
 from .text import compact_whitespace
 
@@ -15,6 +16,27 @@ def duplicate_selected_column_names(selected_column_names, column_names):
             x + suffix for x in selected_column_names):
         suffix += '*'
     return list(column_names) + [x + suffix for x in selected_column_names]
+
+
+def load_csv_safely(path, **kw):
+    try:
+        from pandas import read_csv
+    except ImportError:
+        import pip
+        pip.main('install pandas'.split())
+        from pandas import read_csv
+    if 'skipinitialspace' not in kw:
+        kw['skipinitialspace'] = True
+    f = partial(read_csv, **kw)
+    try:
+        return f(path, encoding='utf-8')
+    except UnicodeDecodeError:
+        pass
+    try:
+        return f(path, encoding='latin-1')
+    except UnicodeDecodeError:
+        pass
+    return f(open(path, errors='replace'))
 
 
 def normalize_key(
